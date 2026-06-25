4bcc5f6b311f2e85200b6535666f2c9aefb78a63
Javier Garcia <oviwanazuldev@gmail.com>
2026-06-25 13:03:33 +0200

    A Row Completed

--- globalcheckbox.js
+++ globalcheckbox.js
@@ -1,5 +1,5 @@
 
-/*  */
+/* A Row  */
 
 const mARow = document.getElementById('mARow');
 const aRowChildren = document.querySelectorAll('.aRow');
@@ -16,6 +16,26 @@ mARow.addEventListener('change', function(e){
       child.checked = false;
     }
 
+  })
+
+})
+
+/* A Yoon Row */
+
+const mAYoonRow = document.getElementById('aRowYoon');
+const aYoonRowChildren = document.querySelectorAll('.aRowYoon');
+
+mAYoonRow.addEventListener('change', function(e){
+
+  const isMAYoonRowChecked = e.target.checked
+
+  aYoonRowChildren.forEach(child => {
+    if (isMAYoonRowChecked) {
+      child.checked = true;
+    }
+    else {
+      child.checked = false;
+    }
   })
 
 })
